08e0eb4b0c351cfafc2aef278f8e20fb1b87c9a0
ffomkartondawalkar <omkar@fitnessforce.com>
2018-04-17 12:26:26 +0530

    f

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+
+var app = express();  
+var server = require('http').createServer(app);  
+app.use(express.static(__dirname + '/node_modules'));  
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+server.listen(3018);  
